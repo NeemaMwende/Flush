3194b7fd6f329f75cac6c1490925d4ee36186e89
Neema <neemamwende009@gmail.com>
2024-09-17 16:17:10 +0300

    react components update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hey there</h1>
+     <Navbar/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;

19e3f552d9a66d549fd1d0466e571a088b159f20
Neema <neemamwende009@gmail.com>
2024-09-20 11:25:41 +0300

    updates

---
+++ flushproject/flushapp/products.py
@@ -0,0 +1,32 @@
+products = [
+    {
+        "_id": 1,
+        "productname": "Camera",
+        "category":"electronics",
+        "price": 5000     
+    },
+    {
+        "_id": 2,
+        "productname": "Laptop",
+        "category":"electronics",
+        "price": 4000     
+    },
+       {
+        "_id": 3,
+        "productname": "Stand",
+        "category":"electronics",
+        "price": 3000     
+    },
+   {
+        "_id": 4,
+        "productname": "Phone",
+        "category":"electronics",
+        "price": 5000     
+    },
+    {
+        "_id": 1,
+        "productname": "LED Light",
+        "category":"electronics",
+        "price": 5000     
+    },
+]
--- flushproject/flushapp/views.py
+++ flushproject/flushapp/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+# from django.http import JsonResponse
+from rest_framework.response import  Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
-def  getRoutes(request):
-    return JsonResponse('Hello Angel', safe=False)
+api_view(['GET'])
+def getRoutes(request):
+    return Response('Hello Angel')
 
+api_view(['GET'])
+def getProducts(request):
+    return Response(products)
